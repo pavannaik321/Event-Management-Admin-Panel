104abc9c2f44c92b0e56ea23a0ae33666219e793
pavan naik <pavanpnaik321@gmail.com>
2024-04-27 14:59:30 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 
 import './App.css'
+import axios from "axios";
+
+axios.defaults.baseURL = "https://event-managment-system-peach.vercel.app";
+// axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
 
 function App() {
+
+  const connectbackend = async()=>{
+    await axios.get("/").then((res)=>{
+      console.log("connection successfull")
+      console.log(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
 
 
   return (
     <>
-
-      <h1>Admin Panel</h1>
-
+    <h1>Admin pannel</h1>
+    <button onClick={()=>connectbackend()} >Check connection</button>
     </>
   )
 }
